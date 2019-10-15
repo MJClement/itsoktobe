16f09c8bb2549ad1259243d12b2e8cab9dfe69a1
Mitchell Clement <mitchellclement@yahoo.co.nz>
2019-10-15 20:12:03 +1300

    MAri

--- js/book.js
+++ js/book.js
@@ -1,5 +1,9 @@
 
+
 $(function() {
+
+
+
 
   function init() {
 
@@ -9,4 +13,39 @@ setTimeout(function(){
 
   }, 5000);
 
+clicks=0;
+animationlistleftbox=["greenbackgroundzoom.gif","waterdroplet.gif","waterdropstwo.gif","water.jpg"]
+    // Toggle bg colour on and off
+    $('.leftbox-top').on('click', function (e) {
+      e.preventDefault();
+    //  $('.leftbox-top').toggleClass('leftbox-top-still leftbox-top-gif');
+    $('.leftbox-top').css('background',"url(assets/"+animationlistleftbox[clicks]+")");
+
+      clicks = clicks+1;
+      console.log(animationlistleftbox[clicks]);
+
+      if(clicks>animationlistleftbox.length-1){
+        clicks=0
+      }
+
+
+// copy and paste - change clicks 1 change name clicks 1 etc on to next box - change name animation rightbox change all instances/tags and then change the 'leftbox'to right box or whatever.
+
+    })
+/*
+//this tells me the left box top on 'click' function targets leftboxtop to toggle the two classes i want to action.
+*/
+
+
+
+    // Toggle bg colour on and off
+    $('.rightbox').on('click', function (e) {
+      e.preventDefault();
+      $('.rightbox').toggleClass('rightbox-still rightbox-gif');
+    })
+
+  }//End of init
+  init();
+
 });
+
