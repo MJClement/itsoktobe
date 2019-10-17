bb2d97be6d4ea5dd68ec1dc7e43833d4299ecfa9
eric <ericmichaelsnell@gmail.com>
2019-10-16 21:55:05 -0700

    loader fixed -- shows on page load then fades out

--- js/book.js
+++ js/book.js
@@ -1,12 +1,16 @@
 
+function init() {
+  const [loader] = document.querySelectorAll('.loadingpage');
 
-// 
-//   function init() {
-//
-// setTimeout(function(){
-//
-//   $(".loadingpage").fadeOut(600)
-//
-// }, 2);
-//
-// }
+  function timeout(ms) {
+    return () => new Promise(resolve => setTimeout(resolve, ms))
+  }
+
+  Promise.resolve()
+    .then(timeout(2000))  // time until fade out animation begins
+    .then(() => loader.classList.add('fadeOut'))
+    .then(timeout(3000))  // time until loader removed from DOM allowing app interactiviy
+    .then(() => loader.style.display = 'none')
+}
+
+init();
